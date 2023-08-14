ab91ce35c7478ed652cfb7317cbf053210c36c22
Mehmet <sumer682000@gmail.com>
2023-08-14 13:31:53 +0300

    remap

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,9 +4,9 @@ vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function() 
 	builtin.grep_string({ search = vim.fn.input("Grep >")});
 end)
-vim.keymap.set('n', '<leader>pd', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>pl', builtin.live_grep, {})
 
 require("telescope").load_extension "file_browser"
 
 vim.keymap.set(
-"n", "<leader>fv",   ":Telescope file_browser path=%:p:h select_buffer=true<CR>", { noremap = true })
+"n", "<leader>pm",   ":Telescope file_browser path=%:p:h select_buffer=true<CR>", { noremap = true })
