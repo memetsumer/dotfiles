3d843114227be874fb342ff325eee504f354c56e
Mehmet <sumer682000@gmail.com>
2024-04-22 10:50:17 +0300

    refactor for tmux

--- after/plugin/barbar.lua
+++ after/plugin/barbar.lua
@@ -2,18 +2,18 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- Move to previous/next
-map('n', '<C-,>', '<Cmd>BufferPrevious<CR>', opts)
-map('n', '<C-.>', '<Cmd>BufferNext<CR>', opts)
+map('n', '<leader><Left>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<leader><Right>', '<Cmd>BufferNext<CR>', opts)
 -- Re-order to previous/next
-map('n', '<C-<>', '<Cmd>BufferMovePrevious<CR>', opts)
-map('n', '<C->>', '<Cmd>BufferMoveNext<CR>', opts)
+map('n', '<leader>bl', '<Cmd>BufferMovePrevious<CR>', opts)
+map('n', '<leader>br', '<Cmd>BufferMoveNext<CR>', opts)
 -- Goto buffer in position...
-map('n', '<C-1>', '<Cmd>BufferGoto 1<CR>', opts)
-map('n', '<C-0>', '<Cmd>BufferLast<CR>', opts)
+map('n', '<leader>1', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<leader>0', '<Cmd>BufferLast<CR>', opts)
 -- Pin/unpin buffer
-map('n', '<C-p>', '<Cmd>BufferPin<CR>', opts)
+map('n', '<leader>bp', '<Cmd>BufferPin<CR>', opts)
 -- Close buffer
-map('n', '<C-c>', '<Cmd>BufferClose<CR>', opts)
+map('n', '<leader>q', '<Cmd>BufferClose<CR>', opts)
 -- Wipeout buffer
 --                 :BufferWipeout
 -- Close commands
